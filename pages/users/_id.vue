6fee22649184b3949f7718869872471635b1376a
Jimmy Richard Timotius Dumalang <jimmy.dumalang@adira.co.id>
2025-02-02 09:50:54 +0700

    dynamic route page - handle serverside and direct call params from template

---
+++ pages/users/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <H1>Hi This is User with ID direct call: {{ $route.params.id }}</H1>
+</template>
+
+<script>
+
+export default {
+  async asyncData(context) {
+    // Server-side console log
+    console.log('Server-side log:', context.params.id);
+
+    // Return data to the component
+    return {};
+  },
+}
+</script>
